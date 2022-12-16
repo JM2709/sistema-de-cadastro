87b6ddf45f04819d71da56b712822606be7461ae
jm <jmmk.contato@gmail.com>
2022-12-16 12:41:06 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistemade gerenciamento de jogos")
+console.log("aplicao esta se conectando ao banco de dados")
+console.log("o paulo fez esse commit sem voce ver na aula")
+console.log("Envio para o github")
